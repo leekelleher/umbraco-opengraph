c6f5d5082f91f0f028cddc90725678841a5be295
leekelleher <leekelleher@gmail.com>
2013-03-20 15:42:47 +0000

    Implemented the initial property editor.

---
+++ Src/Our.Umbraco.DataType.OpenGraph/DataEditor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Our.Umbraco.DataType.OpenGraph
+{
+	[ValidationProperty("Data")]
+	public class DataEditor : WebControl
+	{
+		public DataEditor()
+			: base(HtmlTextWriterTag.Div)
+		{
+			this.CssClass = "gmapContainer";
+		}
+
+		public string SerializedValue
+		{
+			get
+			{
+				if (this.GraphData != null)
+				{
+					// serialize the value
+					var serializer = new JavaScriptSerializer();
+					return serializer.Serialize(this.GraphData);
+				}
+
+				return string.Empty;
+			}
+			set
+			{
+				if (!string.IsNullOrWhiteSpace(value))
+				{
+					// deserialize the value
+					var serializer = new JavaScriptSerializer();
+					this.GraphData = serializer.Deserialize<OpenGraph_Net.OpenGraph>(value);
+				}
+			}
+		}
+
+		public OpenGraph_Net.OpenGraph GraphData { get; set; }
+
+		public HiddenField HiddenValue { get; set; }
+
+		public TextBox Url { get; set; }
+
+		public Button FetchData { get; set; }
+
+		protected override void OnInit(EventArgs e)
+		{
+			base.OnInit(e);
+
+			this.EnsureChildControls();
+
+			this.FetchData.Click += FetchData_Click;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			if (!Page.IsPostBack)
+			{
+				this.HiddenValue.Value = this.SerializedValue;
+
+				if (this.GraphData != null)
+				{
+					this.Url.Text = this.GraphData.OriginalUrl.ToString();
+
+					// TODO: [LK] Present the rest of the OpenGraph data.
+				}
+
+				this.Url.Attributes.Add("placeholder", "http://");
+
+				this.FetchData.Text = "Fetch Open Graph Data";
+			}
+		}
+
+		protected void FetchData_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrWhiteSpace(this.Url.Text) && this.Url.Text.StartsWith("http"))
+			{
+				this.GraphData = OpenGraph_Net.OpenGraph.ParseUrl(this.Url.Text);
+				this.HiddenValue.Value = this.SerializedValue;
+			}
+		}
+
+		protected override void CreateChildControls()
+		{
+			base.CreateChildControls();
+
+			this.HiddenValue = new HiddenField() { ID = "HiddenValue" };
+			this.Url = new TextBox() { ID = "Url", CssClass = "umbEditorTextField" };
+			this.FetchData = new Button() { ID = "FetchData" };
+
+			this.Controls.Add(this.HiddenValue);
+			this.Controls.Add(this.Url);
+			this.Controls.Add(this.FetchData);
+		}
+	}
+}
---
+++ Src/Our.Umbraco.DataType.OpenGraph/DataType.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using umbraco.cms.businesslogic.datatype;
+using umbraco.editorControls.SettingControls;
+
+namespace Our.Umbraco.DataType.OpenGraph
+{
+	public class DataType : AbstractDataEditor
+	{
+		public const string DataTypeGuid = "8DF8A0EE-496F-4268-81DA-E937E376CDBE";
+
+		private DataEditor m_Control = new DataEditor();
+
+		public DataType()
+		{
+			// set the render control as the placeholder
+			this.RenderControl = this.m_Control;
+
+			// assign the initialise event for the control
+			this.m_Control.Init += new EventHandler(this.m_Control_Init);
+
+			// assign the value to the control
+			this.m_Control.PreRender += new EventHandler(this.m_Control_PreRender);
+
+			// assign the save event for the data-type/editor
+			this.DataEditorControl.OnSave += new AbstractDataEditorControl.SaveEventHandler(this.DataEditorControl_OnSave);
+		}
+
+		public override Guid Id
+		{
+			get
+			{
+				return new Guid(DataTypeGuid);
+			}
+		}
+
+		public override string DataTypeName
+		{
+			get
+			{
+				return "Open Graph";
+			}
+		}
+
+		//[DataEditorSetting("Default Location", defaultValue = Constants.DefaultCoordinates, type = typeof(TextField))]
+		//public string DefaultLocation { get; set; }
+
+		private void m_Control_Init(object sender, EventArgs e)
+		{
+			// get the options from the Prevalue Editor.
+			this.m_Control.SerializedValue = "";
+		}
+
+		private void m_Control_PreRender(object sender, EventArgs e)
+		{
+			this.m_Control.SerializedValue = this.Data.Value != null ? this.Data.Value.ToString() : string.Empty;
+		}
+
+		private void DataEditorControl_OnSave(EventArgs e)
+		{
+			this.Data.Value = this.m_Control.SerializedValue;
+		}
+	}
+}
