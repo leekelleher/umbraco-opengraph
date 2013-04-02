cd1fbd4a0805a20f7c2460669e922c85a71b67f2
leekelleher <leekelleher@gmail.com>
2013-04-02 15:36:45 +0100

    Using ILMerge via NuGet.

--- Src/Our.Umbraco.DataType.OpenGraph/Properties/VersionInfo.cs
+++ Src/Our.Umbraco.DataType.OpenGraph/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.10.33592")]
+[assembly: AssemblyFileVersion("1.0.14.42235")]
 [assembly: AssemblyInformationalVersion("1.0.0-beta")]
 
 
